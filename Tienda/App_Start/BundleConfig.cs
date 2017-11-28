5d26bf7b5f4355cc5c904e0cbc9dfe7b7a004672
Oscar irribarra <osc-r@live.cl>
2017-11-27 21:10:29 -0300

    Agregado Filtrado de Tablas

--- Tienda/App_Start/BundleConfig.cs
+++ Tienda/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@ namespace Tienda
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Content/Scripts/jquery.validate*",
                         "~/Content/Scripts/error-jquery.js"));
-   
+
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -27,28 +27,29 @@ namespace Tienda
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Content/Scripts/bootstrap.min.js",
                       "~/Content/Scripts/respond.js"));
-                        
+
 
             ScriptBundle LteJS = new ScriptBundle("~/bundles/AdminLteJs");
             LteJS.Orderer = new DefinedBundleOrderer();
             LteJS.Include(
-                "~/Content/Scripts/AdminLte/raphael.min.js",              
+                "~/Content/Scripts/AdminLte/raphael.min.js",
                 "~/Content/Scripts/AdminLte/jquery.sparkline.min.js",
                 "~/Content/Scripts/AdminLte/jquery-jvectormap-1.2.2.min.js",
                 "~/Content/Scripts/AdminLte/jquery.knob.min.js",
-                "~/Content/Scripts/AdminLte/moment.min.js",              
+                "~/Content/Scripts/AdminLte/moment.min.js",
                 "~/Content/Scripts/AdminLte/bootstrap3-wysihtml5.all.min.js",
                 "~/Content/Scripts/AdminLte/jquery.slimscroll.min.js",
                 "~/Content/Scripts/AdminLte/fastclick.js",
                 "~/Content/Scripts/AdminLte/adminlte.min.js",
                 "~/Content/Scripts/AdminLte/demo.js");
+
             bundles.Add(LteJS);
-            
-            
+
+
         }
 
     }
 
-   
+
 }
 
